cf08737bb34165b87bbcff58d0e759d4b082a271
Animatic_Almanac <67105502+CambellJSmith@users.noreply.github.com>
2026-09-14 14:58:28 +0100

    preserve legacy competition identifiers

--- legacy-competition-compat.js
+++ legacy-competition-compat.js
@@ -1,9 +1,26 @@
 (function () {
   "use strict";
 
-  const LEAGUES = Object.freeze([Object.freeze({ id: "local" })]);
-  const DIFFICULTIES = Object.freeze([Object.freeze({ id: "standard" })]);
-  const ARCHETYPES = Object.freeze([Object.freeze({ id: "endurance" })]);
+  const LEAGUES = Object.freeze([
+    Object.freeze({ id: "local" }),
+    Object.freeze({ id: "bronze" }),
+    Object.freeze({ id: "silver" }),
+    Object.freeze({ id: "gold" }),
+    Object.freeze({ id: "master" })
+  ]);
+  const DIFFICULTIES = Object.freeze([
+    Object.freeze({ id: "cautious" }),
+    Object.freeze({ id: "standard" }),
+    Object.freeze({ id: "elite" })
+  ]);
+  const ARCHETYPES = Object.freeze([
+    Object.freeze({ id: "endurance" }),
+    Object.freeze({ id: "powerhouse" }),
+    Object.freeze({ id: "fortress" }),
+    Object.freeze({ id: "mystic" }),
+    Object.freeze({ id: "guardian" }),
+    Object.freeze({ id: "sprinter" })
+  ]);
 
   window.PocketHatcheryCompetitionEngine = Object.freeze({
     LEAGUES,
